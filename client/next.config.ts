82daf51e2091272d4364249b3b1f495b3ab28b17
Precious Adedokun <apreezofficial@gmail.com>
2025-10-29 20:55:13 +0100

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,9 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
