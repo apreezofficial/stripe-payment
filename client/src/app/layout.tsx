36eef9337c4ac19c615ab60409969fde6dfd9317
Precious Adedokun <apreezofficial@gmail.com>
2025-10-30 16:42:56 +0100

    Update layout.tsx

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -3,14 +3,16 @@ import { Inter } from 'next/font/google';
 import { CartProvider } from '@/context/CartContext'; // <-- Import the provider
 import { Toaster } from 'sonner'; // <-- Recommended for showing toasts
 import "./globals.css";
-
+import Navbar from '@/components/layout/Navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+       
         <CartProvider> {/* <--- CRITICAL: Wrap the content here! */}
+         <Navbar />
           {children}
           <Toaster position="bottom-right" richColors /> {/* For seeing "Item added" messages */}
         </CartProvider> {/* <--- CRITICAL: Close the Provider here! */}
